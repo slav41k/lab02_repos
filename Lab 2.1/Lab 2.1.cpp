84639c24d35097baaa50d293a1a5cb3594e064f0
slav41k <yaroslav.teslitskyi.ri.2022@lpnu.ua>
2022-10-07 02:51:44 +0300

    2comm

--- Lab 2.1/Lab 2.1.cpp
+++ Lab 2.1/Lab 2.1.cpp
@@ -7,15 +7,15 @@ int main()
 	double Pi = 4 * atan(1.); // число пі
 	double a; // вхідний параметр
 	double z1; // результат обчислення 1-го виразу
-	//double z2; // результат обчислення 2-го виразу
+	double z2; // результат обчислення 2-го виразу
 
 	cout << "a = "; cin >> a;
 	z1 = 2 * (sin(3 * Pi - 2 * a) * sin(3 * Pi - 2 * a)) * (cos(5 * Pi + 2 * a) * cos(5 * Pi + 2 * a));
-	//z2 = 0.25 - 0.25 * sin(2.5 * Pi - 8 * a);
+	z2 = 0.25 - 0.25 * sin(2.5 * Pi - 8 * a);
 	
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	//cout << "z2 = " << z2 << endl;
+	cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
